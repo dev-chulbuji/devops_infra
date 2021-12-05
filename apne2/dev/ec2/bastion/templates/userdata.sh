d8541f39191dc0e6b07c859dfc2d6aeac1c62833
dj.kim <dj.kim@toss.im>
2021-12-05 20:22:31 +0900

    wip

--- apne2/dev/ec2/bastion/templates/userdata.sh
+++ apne2/dev/ec2/bastion/templates/userdata.sh
@@ -3,6 +3,11 @@
 # install cwagent
 sudo yum install -y amazon-cloudwatch-agent
 
+# download config
+wget \
+  https://raw.githubusercontent.com/dev-chulbuji/devops_infra/master/apne2/dev/ec2/bastion/templates/cloudwatch-agent-config.json \
+  -O /opt/aws/amazon-cloudwatch-agent/bin/config.json
+
 # run agent
 sudo amazon-cloudwatch-agent-ctl \
   -a fetch-config \
